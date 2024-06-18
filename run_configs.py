8b071828a8b72777dd8bc20c753c9495eba64417
Harold Caballero <mati_67m@hotmail.com>
2024-06-17 20:51:44 -0400

    agregado multiprocesado a ejecucion de the one

---
+++ run_configs.py
@@ -0,0 +1,47 @@
+import glob
+import subprocess
+from multiprocessing import Pool
+
+
+def run_one(path: str):
+    try:
+        subprocess.run(
+            [
+                "java",
+                "-Xmx512M",
+                "-cp",
+                "target:lib/ECLA.jar:lib/DTNConsoleConnection.jar",
+                "core.DTNSim",
+                "-b",
+                "1",
+                path,
+            ],
+            capture_output=True,
+            check=True,
+        )
+        print("flag2")
+        return f"Logrado: {path}"
+    except subprocess.CalledProcessError as e:
+        with open("errors/" + path.split("/")[1], "a") as file:
+            file.writelines(
+                [
+                    f"Command '{e.cmd}' exited with non-zero exit status {e.returncode}.",
+                    f"Output: {e.stderr}",
+                    f"{e.stdout}",
+                    "-----------------------------\n",
+                ]
+            )
+        return f"Error: {path}"
+    except Exception as e:
+        print("ERROR!")
+        with open("errors/" + path.split("/")[1], "a") as file:
+            file.writelines([e, "-----------------------------\n"])
+        return f"Error: {path}"
+
+
+if __name__ == "__main__":
+    print("Empezando")
+    with Pool() as pool:
+        res = pool.imap_unordered(run_one, glob.glob("configs/config_*.txt"))
+        for r in res:
+            print(r)
